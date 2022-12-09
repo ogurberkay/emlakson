5e86df3d1fd6b79516afb6f3cd1fe1b6179711e3
Rıza Mert Yağcı <rizamertyagci@gmail.com>
2022-12-10 01:13:34 +0300

    .

--- Core/Extensions/StringExtensions.cs
+++ Core/Extensions/StringExtensions.cs
@@ -4,6 +4,15 @@ namespace Core.Extensions;
 
 public static class StringExtensions
 {
+    public static string GetUniqueFileName(string fileName)
+    {
+        fileName = Path.GetFileName(fileName);
+        return string.Concat(Path.GetFileNameWithoutExtension(fileName)
+                            , "_"
+                            , Guid.NewGuid().ToString().AsSpan(0, 8)
+                            , Path.GetExtension(fileName));
+    }
+
     public static string TurkishChrToEnglishChr(this string text)
     {
         if (string.IsNullOrEmpty(text)) return text;
